b2c428b4a493a29c797445a8b570f4c50eefaef2
uthayathasan <uthayathasan@yahoo.com>
2018-03-21 18:47:59 +0530

    21March2018_6_47PM

--- ClientApp/app/Till/eposTransactionRepository.ts
+++ ClientApp/app/Till/eposTransactionRepository.ts
@@ -91,11 +91,13 @@ export class EposTransactionRepository{
         .subscribe(
             response => {
                 this.tRepo.item = response;
+                let canAdd=true;
                 if(id==this.tRepo.item.itemNo){
                     if(this.tRepo.item.priceEntry){
                         let price=parseInt(this.cart.journalInput);
                         if((price==0)||(this.cart.journalInput=="")){
                             this.cart.journalText="Please Enter the Price";
+                            canAdd=false;
                         }else{
                             if(this.cart.qty==0){this.cart.qty=1;}
                             this.cart.price=price/100.0;
@@ -104,48 +106,90 @@ export class EposTransactionRepository{
                         }
                     }else{
                         if(this.cart.qty==0){this.cart.qty=1;}
-                        this.cart.price=this.tRepo.item.price;
+                        if(this.cart.orderType==0){
+                            this.cart.price=this.tRepo.item.price;
+                        }else if(this.cart.orderType==1){
+                            this.cart.price=this.tRepo.item.takeawayPrice;
+                        }else if(this.cart.orderType==2){
+                            this.cart.price=this.tRepo.item.deliveryPrice;
+                        }
+                        if(this.cart.price==0){
+                            this.cart.price=this.tRepo.item.price;
+                        }
                         this.cart.journalText=this.tRepo.item.description +" @ "+(this.cart.qty*this.cart.price).toFixed(2);
                     }
-                    let m=new EposTransLine();
-                    m.amount=this.cart.qty*this.cart.price;
-                    m.barcode=this.tRepo.item.barcode;
-                    m.departmentId=this.tRepo.item.department;
-                    m.description=this.tRepo.item.description;
-                    m.entryType=0;
-                    m.itemGroup=this.tRepo.item.itemGroup;
-                    m.itemSubGroup=this.tRepo.item.itemSubGroup;
-                    m.lineNo=this.cart.getMaxLineNo()+1;
-                    m.netAmount=this.cart.qty*this.cart.price;
-                    m.number=this.tRepo.item.itemNo;
-                    m.orderType=this.cart.orderType;
-                    m.price=this.cart.price;
-                    m.printGroup=this.tRepo.item.printGroup;
-                    m.quantity=this.cart.qty;
-                    m.staffId=this.repo.logedInStaff.userId;
-                    m.storeId=this.repo.device.storeId;
-                    m.tillId=this.repo.device.tillId;
-                    m.totalCost=this.tRepo.item.unitCost*this.cart.qty;
-                    m.transDate= this.repo.currentDateTime;
-                    m.transId=this.cart.slipNo;
-                    m.unitCost=this.tRepo.item.unitCost;
-                    m.vatCode=this.tRepo.item.vatCode;
-                    m.barPrintedTime= this.repo.minDate;
-                    m.kitchenPrintedTime=this.repo.minDate;                        
-                    let result=0;
-                    let url=eposTransLinesUrl;
-                    url +="?customerId="+this.repo.filter.customerId;
-                    url +="&storeId="+this.repo.filter.storeId;
-                    url +="&tillId="+this.repo.filter.tillId;
-                    this.repo.sendRequest(RequestMethod.Post, url, m).subscribe(response => {
-                        result = response;
-                        if(result>0){
-                            this.tRepo.getEposTransLines(this.cart.slipNo);
-                            this.cart.qty=0;
-                            this.cart.price=0;
-                            this.cart.journalInput="";
+                    if(canAdd){
+                        if(this.cart.transType==1){this.cart.qty=-1*this.cart.qty}
+                        let m=new EposTransLine();
+                        m.amount=this.cart.qty*this.cart.price;
+                        m.barcode=this.tRepo.item.barcode;
+                        m.barPrint=this.tRepo.item.barPrint;
+                        m.barPrinted=false;
+                        m.barPrintedTime=this.repo.minDate;
+                        m.departmentId=this.tRepo.item.department;
+                        m.description=this.tRepo.item.description;
+                        m.discountAmount=0;
+                        m.discountable=this.tRepo.item.discountAllowed;
+                        m.discountPercentage=0;
+                        m.elno=0;
+                        m.entryType=0;
+                        m.freeText="";
+                        m.isChange=false;
+                        m.isModifier=false;
+                        m.isRefund=false;
+                        if(this.cart.transType==1){
+                            m.isRefund=true;
                         }
-                    });
+                        m.itemGroup=this.tRepo.item.itemGroup;
+                        m.itemSubGroup=this.tRepo.item.itemSubGroup;
+                        m.kitchenPrint=this.tRepo.item.kitchenPrint;
+                        m.kitchenPrinted=false;
+                        m.kitchenPrintedTime=this.repo.minDate;
+                        m.lineNo=this.cart.getMaxLineNo()+1;
+                        m.lineStatus=false;
+                        m.linkedOfferId=0;
+                        m.mainItemNo="";
+                        m.mandatory=false;
+                        m.netAmount=this.cart.qty*this.cart.price;
+                        m.number=this.tRepo.item.itemNo;
+                        m.offerID="";
+                        m.offerQuantity=0;
+                        m.offerTrigger=false;
+                        m.orderType=this.cart.orderType;
+                        m.paymentType=0;
+                        m.price=this.cart.price;
+                        m.printGroup=this.tRepo.item.printGroup;
+                        m.quantity=this.cart.qty;
+                        m.scale=this.tRepo.item.scale;
+                        m.scanned=false;
+                        m.served=false;
+                        m.splitGroup=false;
+                        m.staffId=this.repo.logedInStaff.userId;
+                        m.storeId=this.repo.device.storeId;
+                        m.tempItem=false;
+                        m.tillId=this.repo.device.tillId;
+                        m.totalCost=this.tRepo.item.unitCost*this.cart.qty;
+                        m.transDate= this.repo.currentDateTime;
+                        m.transId=this.cart.slipNo;
+                        m.unitCost=this.tRepo.item.unitCost;
+                        //m.vatAmount
+                        m.vatCode=this.tRepo.item.vatCode;
+                        //m.vatRate
+                        let result=0;
+                        let url=eposTransLinesUrl;
+                        url +="?customerId="+this.repo.filter.customerId;
+                        url +="&storeId="+this.repo.filter.storeId;
+                        url +="&tillId="+this.repo.filter.tillId;
+                        this.repo.sendRequest(RequestMethod.Post, url, m).subscribe(response => {
+                            result = response;
+                            if(result>0){
+                                this.tRepo.getEposTransLines(this.cart.slipNo);
+                                this.cart.qty=0;
+                                this.cart.price=0;
+                                this.cart.journalInput="";
+                            }
+                        });
+                    }
                 }
                 else{
                 this.cart.journalText="Item Not Found";
